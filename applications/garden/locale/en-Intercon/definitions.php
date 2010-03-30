d4cc1c2df59d22d66d32c373daccf1cbfd29c548
Nat Budin <natbudin@gmail.com>
2010-03-30 09:25:05 -0700

    Add Intercon-specific code

---
+++ applications/garden/locale/en-Intercon/definitions.php
@@ -0,0 +1,9 @@
+<?php
+require_once("applications/garden/locale/en-CA/definitions.php");
+
+$Definition["Apply for Membership"] = "Register for Intercon";
+$Definition["Howdy, Stranger!"] = "Members only";
+$Definition["It looks like you're new here. If you want to take part in the discussions, click one of these buttons!"] = 
+  "This forum is only available to registered Intercon attendees.  To join the discussion, log in on the Intercon site ".
+  "and then come back here.";
+?>
