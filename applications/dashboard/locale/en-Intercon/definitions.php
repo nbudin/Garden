9f181af98ce4950728acc52612ce3b8ab0fda546
Nat Budin <natbudin@gmail.com>
2010-08-24 15:18:26 -0400

    Better error message (although it's slightly a prevarication) about account edits

--- applications/dashboard/locale/en-Intercon/definitions.php
+++ applications/dashboard/locale/en-Intercon/definitions.php
@@ -6,4 +6,6 @@ $Definition["Howdy, Stranger!"] = "Members only";
 $Definition["It looks like you're new here. If you want to take part in the discussions, click one of these buttons!"] = 
   "This forum is only available to registered Intercon attendees.  To join the discussion, log in on the Intercon site ".
   "and then come back here.";
+$Definition["Username can only contain letters, numbers, underscores, and must be between 3 and 20 characters long."] =
+  "Sorry, you can't edit your user account on this site.  To make profile changes, please go to the main Intercon site.";
 ?>
